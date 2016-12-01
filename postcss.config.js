74477088549939268dfc34ef7b0256eb762ab7f8
Egor Avakumov <avakymov@yandex.ru>
2016-12-02 03:44:35 +0700

    Добавлены шрифты, добавлен postcss

---
+++ postcss.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'use': ['postcss-cssnext', 'postcss-focus', 'postcss-pxtorem', 'postcss-inline-svg', 'cssnano'],
+  'input': './static/styles/main.css',
+  'output': './static/styles/main.css',
+  'local-plugins': true,
+  'cssnano': {
+    'discardComments': {
+      'removeAll': true
+    }
+  },
+  'postcss-pxtorem': {
+    'rootValue': 14,
+    'replace': false,
+    'selectorBlackList': ['body', 'html']
+  },
+  /*'postcss-sprites': {
+    'stylesheetPath': './dist/css',
+    'spritePath': './dist/css/sprites',
+    'basePath': './dist/images',
+    filterBy: function(image) {
+      // Allow only png files
+      if (!/\.png$/.test(image.url)) {
+        return Promise.reject();
+      }
+      return Promise.resolve();
+    }
+  },
+  'postcss-font-magician': {
+    'foundries': 'bootstrap google',
+    'hosted': './dist/fonts'
+  }*/
+};
--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,8 @@ $(document).ready(function() {
 
   $asideSlider.owlCarousel({
     items: 1,
-    loop: true
+    loop: true,
+    dots: true
   });
 
   $('.aside-slider-left').click(function() {
@@ -27,4 +28,46 @@ $(document).ready(function() {
   $('.aside-slider-right').click(function() {
       $asideSlider.trigger('next.owl.carousel');
   })
+  $asideSlider.on('changed.owl.carousel', function(e) { setTab(e.item.index) });
+
+  var $tabs = $('.aside-slider .btns .btn');
+  $tabs.on('click', function(e) {
+    e.preventDefault();
+    var text = $(this).attr('href');
+    text = text.slice(1);
+    var indexEl = 0;
+     var $element = document.querySelector('.owl-stage .owl-item:not(.cloned) .item[data-tab="'+text+'"]')
+     
+     var owlItems = document.querySelectorAll('.owl-stage .owl-item:not(.cloned)');
+
+      [].forEach.call(owlItems, function(el, i) {
+        if( el === $element.parentNode ) {
+          indexEl = i;
+        }
+      });
+
+    $asideSlider.trigger('to.owl.carousel', [indexEl, 300]);
+    $asideSlider.trigger('changed.owl.carousel');
+
+    // setTab(indexEl);
+  });
+
+
+
+  function setTab(i) {
+  
+  var index = i + 1;
+
+  var $element = $('.owl-stage').children('.owl-item:nth-child(' + index + ')');
+
+  var text = $element.children('.item').attr('data-tab');
+  text = '#' + text;
+  console.log($tabs);
+  $tabs.each(function(i, el) {
+    $(el).addClass('-disabled');
+    if( $(el).attr('href') === text ) {
+      $(el).removeClass('-disabled');
+    }
+  });
+}
 });
